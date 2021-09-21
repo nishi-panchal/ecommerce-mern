c7ff271e409aba17f75b78991fea8a2eed25d62a
nishipanchal97 <nishipanchal97@gmail.com>
2021-09-21 15:22:22 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ecommerce built on MERN 
+          Ecommerce built on MERN stack and it's going to be awesome
         </p>
       </header>
     </div>
